15bca5a1a23291aad4dcff8704679c7160c7e9ce
Nikitulka0120 <107490259+Nikitulka0120@users.noreply.github.com>
2025-02-23 13:55:54 +0700

    Update shaker.c

--- shaker.c
+++ shaker.c
@@ -60,7 +60,6 @@ void PrintMas(int n, int A[])
     }
     printf("\n");
 }
-
 int bubbleSort(int n, int A[])
 {
     // printf("Контрольная сумма до сортировки:%d\n",CheckSum(n, A));
@@ -89,26 +88,138 @@ int bubbleSort(int n, int A[])
     return trud;
 }
 
+int ShakerSortPrint(int n, int A[])
+{
+    printf("Контрольная сумма до сортировки:%d\n", CheckSum(n, A));
+    printf("Число серий до сортировки:%d\n", RunNumber(n, A));
+    int C = 0;
+    int M = 0;
+    int L = 0;
+    int R = n - 1;
+    int k = n - 1;
+    do
+    {
+        for (int j = R; j > L; j--)
+        {
+            if (A[j] < A[j - 1])
+            {
+                M++;
+                int temp = A[j];
+                M++;
+                A[j] = A[j - 1];
+                M++;
+                A[j - 1] = temp;
+                k = j;
+            }
+        }
+        L = k;
+        for (int j = L; j < R; j++)
+        {
+            if (A[j] > A[j + 1])
+            {
+                M++;
+                int temp = A[j];
+                M++;
+                A[j] = A[j + 1];
+                M++;
+                A[j + 1] = temp;
+                k = j;
+            }
+        }
+        R = k;
+    } while (L < R);
+    printf("Контрольная сумма после сортировки:%d\n", CheckSum(n, A));
+    printf("Число серий после сортировки:%d\n", RunNumber(n, A));
+    int trud = M + C;
+    return trud;
+}
+
+int ShakerSort(int n, int A[])
+{
+    int C = 0;
+    int M = 0;
+    int L = 0;
+    int R = n - 1;
+    int k = n - 1;
+    do
+    {
+        for (int j = R; j > L; j--)
+        {
+            C++;
+            if (A[j] < A[j - 1])
+            {
+                M++;
+                int temp = A[j];
+                M++;
+                A[j] = A[j - 1];
+                M++;
+                A[j - 1] = temp;
+                k = j;
+            }
+        }
+        L = k;
+        for (int j = L; j < R; j++)
+        {   
+            C++;
+            if (A[j] > A[j + 1])
+            {
+                M++;
+                int temp = A[j];
+                M++;
+                A[j] = A[j + 1];
+                M++;
+                A[j + 1] = temp;
+                k = j;
+            }
+        }
+        R = k;
+    } while (L < R);
+    int trud = M + C;
+    return trud;
+}
+
 int main()
 {
-    printf("+---------------+----------------------------+--------------------------+--------------------------+-----------------------------+\n");
-    printf("|    N (size)   | Теоретическая Трудоемкость | Трудоемкость (убывающий) | Трудоемкость (случайный) | Трудоемкость (возрастающий) |\n");
-    printf("+---------------+----------------------------+--------------------------+--------------------------+-----------------------------+\n");
 
     for (int i = 1; i < 6; i++)
     {
-        int trud;
         int n = 100 * i;
         int A[n];
-        int theor = 3 * (n * (n - 1) / 2) + (n * (n - 1) / 2);
+        printf("----------------------------------- Убывающий для %d эдементов -------------------------------------\n", n);
         FillDec(n, A);
-        trud = bubbleSort(n, A);
+        ShakerSortPrint(n, A);
+        printf("----------------------------------- Случайный для %d эдементов -------------------------------------\n", n);
         FillRand(n, A);
-        int trud_rand = bubbleSort(n, A);
+        ShakerSortPrint(n, A);
+        printf("----------------------------------- Возрастающий для %d эдементов -------------------------------------\n", n);
         FillInc(n, A);
-        int trud_inc = bubbleSort(n, A);
-        printf("| %12d  | %26d | %24d | %24d | %27d |\n", n, theor, trud, trud_rand, trud_inc);
+        ShakerSortPrint(n, A);
+    }
+    printf("+---------+--------------------------------------+--------------------------------------+\n");
+    printf("+         |       Пузырьковая сортировка         |         Шейкерная сортировка         |\n");
+    printf("+    N    +-----------+-----------+--------------+-----------+--------------+-----------+\n");
+    printf("|         | убывающий | случайный | возрастающий | убывающий | случайный | возрастающий |\n");
+    printf("+---------+-----------+-----------+--------------+-----------+--------------+-----------+\n");
+
+    for (int i = 1; i < 6; i++)
+    {
+        int strud;
+        int n = 100 * i;
+        int A[n];
+        FillDec(n, A);
+        strud = ShakerSort(n, A);
+        FillRand(n, A);
+        int strud_rand = ShakerSort(n, A);
+        FillInc(n, A);
+        int strud_inc = ShakerSort(n, A);
+        FillDec(n, A);
+        int btrud = bubbleSort(n, A);
+        FillRand(n, A);
+        int btrud_rand = bubbleSort(n, A);
+        FillInc(n, A);
+        int btrud_inc = bubbleSort(n, A);
+        printf("| %6d  | %9d | %9d | %12d | %9d | %9d | %12d |\n", n, btrud, btrud_rand, btrud_inc, strud, strud_rand, strud_inc);
     }
 
-    printf("+---------------+----------------------------+--------------------------+--------------------------+-----------------------------+\n");
+    printf("+---------+-----------+-----------+--------------+-----------+--------------+-----------+\n");
 }
